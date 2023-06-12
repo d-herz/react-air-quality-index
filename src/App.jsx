f1e8ba1ef50a93d96b74553adbe98ee812dc2851
Daniel Hyres <danhyres@gmail.com>
2023-06-12 17:58:01 -0400

    Updates to App.jsx, main.jsx, index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react' 
+import React from 'react' 
 import './App.css'
 
+// Fetch to Air Quality Open Data Platform
+React.useEffect(() => {
+  async function getAirQualityData(city) {
+    try {
+      const res = await fetch(`https://api.waqi.info/feed/${city}/?token=${import.meta.env.VITE_API_KEY}`)
+      const data = await res.json()
+
+    } catch {
+
+    }
+    
+
+  }
+
+
+},[])
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <h1>Hello world</h1>
   )
 }
 
