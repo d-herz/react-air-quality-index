3667ef61a4419d41751c84723f4de03a192c2d3c
Daniel Hyres <danhyres@gmail.com>
2023-06-13 15:31:36 -0400

    Updating components, adding City Search Form component

---
+++ components/CitySearchForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CitySearchForm(props) {
+  return (
+    <div>CitySearchForm</div>
+  )
+}
+
+export default CitySearchForm
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
 import React from 'react' 
 import './App.css'
-
-// Fetch to Air Quality Open Data Platform
-React.useEffect(() => {
-  async function getAirQualityData(city) {
-    try {
-      const res = await fetch(`https://api.waqi.info/feed/${city}/?token=${import.meta.env.VITE_API_KEY}`)
-      const data = await res.json()
-
-    } catch {
-
-    }
-    
-
-  }
-
-
-},[])
+import CitySearchForm from '../components/CitySearchForm'
 
 function App() {
 
+  const [error, setError] = React.useState(null)
+  const [airQualityData, setAirQualityData] = React.useState(null)
+
+  // Fetch to Air Quality Open Data Platform
+  React.useEffect(() => {
+    const token = import.meta.env.VITE_API_KEY
+    async function getAirQualityData(city) {
+      try {
+        const res = await fetch(`https://api.waqi.info/feed/${city}/?token=${token}`)
+        const data = await res.json()
+        console.log(res)
+        console.log(data)
+        if (res.ok && data.status === "ok") {
+          setAirQualityData(data.data)
+          setError(null)
+        } else {
+          setError("Sorry, location not found. Please check your spelling and try again.")
+          setAirQualityData(null)
+        }
+
+      } catch (error) {
+        console.error("network error:", error)
+        setError("Something went wrong!")
+        setAirQualityData(null)
+      }
+  
+    }
+    getAirQualityData("here")
+  
+  },[])
 
   return (
-   <h1>Hello world</h1>
+    <div>
+      <h1>Air Quality Index Checker</h1>
+      <CitySearchForm
+        getAirQualityData = {getAirQualityData}
+      />
+    </div>
+    
   )
 }
 
